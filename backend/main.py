5e9cf622d8deddbba45cfd3eb34fc77e26ceb80a
Ronit Shah <ronitshah0099@gmail.com>
2026-05-13 12:41:26 +0530

    CORS Error

--- backend/main.py
+++ backend/main.py
@@ -98,7 +98,10 @@ app.add_middleware(SecurityHeadersMiddleware)
 # 2. CORS second (Outermost - handles preflight)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://digital-twin-ten-sand.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
