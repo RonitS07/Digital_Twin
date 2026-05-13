39ef9507ecc284f7cdeccb404c9779d0f32ae689
Ronit Shah <ronitshah0099@gmail.com>
2026-05-13 11:14:08 +0530

    Allow Firebase token header in CORS

--- backend/main.py
+++ backend/main.py
@@ -91,7 +91,7 @@ app.add_middleware(
     allow_origins=settings.BACKEND_CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*", "X-Firebase-Token"],
     expose_headers=["*"],
 )
 
